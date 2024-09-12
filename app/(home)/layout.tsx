c978c7e85c13d3f4f9d686136731cd58a7edeb66
Manish Suthar <manishsuthar078@gmail.com>
2024-09-13 00:12:08 +0530

    chore: init file structure

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import React from "react";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+export const metadata: Metadata = {
+  title: "GenZ Dictionary",
+  description: "A Dictionary for genZ generation.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
